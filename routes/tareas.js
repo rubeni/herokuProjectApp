1be9934b88eb03f87497df2fdec59fb1d9d24b81
Ruben Uroz <ruben.uroz@gmail.com>
2017-03-27 19:23:55 +0200

    ENTREGA FASE 1

---
+++ routes/tareas.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+//array de tareas
+var tareas = [
+	{
+		"nom":"Diseño wireframe",
+		"etiquetes": [ "wireframe","app","hibrida"],
+		"realitzacio":"36"
+	},
+	{
+		"nom":"Reunión",
+		"etiquetes": ["app","hibrida"],
+		"realitzacio":"88"
+	},
+	{
+		"nom":"Desarrollo Layout",
+		"etiquetes": [ "layout","app","hibrida"],
+		"realitzacio":"25"
+	},
+	{
+		"nom":"Reunión cliente",
+		"etiquetes": [ "reunión","app","hibrida"],
+		"realitzacio":"40"
+	}
+]
+
+/* GET tareas page. */
+router.get('/', function(req,res,next) {
+ res.render('tareas_lista', { title: 'Tareas List/Index', tareas: tareas});
+});
+
+/* GET new tareas page. */
+router.get('/new', function(req,res,next) {
+ res.render('tareas_new', { title: 'TareasNew' });
+});
+
+/* GET crear tareas page. */
+router.post('/crear', function(req,res,next) {
+ res.render('tareas_crear', { title: 'TareasCrear' });
+});
+
+module.exports = router;
